9cac993981c112843573ca0f5ef7966269282402
dtun <dtun@me.com>
2023-11-15 20:39:56 -0700

    Wire up api

---
+++ api/pokeapi.ts
@@ -0,0 +1,19 @@
+export interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export const getPokemon = async (limit = 150): Promise<Pokemon[]> => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
+  );
+  const data = await response.json();
+
+  return data.results.map((p: Pokemon, index: number) => ({
+    ...p,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`,
+  }));
+};
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,21 @@
 import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Pokemon, getPokemon } from '@/api/pokeapi';
 
 const Page = () => {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const result = await getPokemon();
+
+      setPokemon(result);
+    };
+
+    load();
+  }, []);
+
   return (
     <View>
       <Link href="./[pokemon]/test">To [pokemon]/test</Link>
